6ddee142131e460ee2185974c9dc065e86bb2df9
matekuna92 <kunamate66@gmail.com>
2022-11-26 15:35:26 +0100

    Props date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,19 @@ import React from 'react';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    // expenseDate is a date object, cant be output as text: toISOString()
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
 
+    // expenseDate is a date object, cant be output as text: toISOString()
     return (
         <div className="expense-item">
-            <div> {props.date.toISOString()} </div>        
+            <div>
+            <div> {month} </div>
+            <div> {year} </div>
+            <div> {day} </div>
+            {props.date.toISOString()}
+            </div>        
             <div className="expense-item__description"> 
             <h2> {props.title} </h2>
             <div className="expense-item__price"> ${props.amount}</div>
