5cd585787b29f693e53d504eb8689fe0b7f207da
matekuna92 <kunamate66@gmail.com>
2022-11-26 15:27:06 +0100

    Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 3, 28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
     // expenseDate is a date object, cant be output as text: toISOString()
 
     return (
         <div className="expense-item">
-            <div> {expenseDate.toISOString()} </div>        
+            <div> {props.date.toISOString()} </div>        
             <div className="expense-item__description"> 
-            <h2> {expenseTitle} </h2>
-            <div className="expense-item__price"> ${expenseAmount}</div>
+            <h2> {props.title} </h2>
+            <div className="expense-item__price"> ${props.amount}</div>
             </div>
         </div>
     );
