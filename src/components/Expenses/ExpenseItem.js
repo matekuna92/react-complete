cae66a9a90eb3f58bf5a545f5bc1e8cf9d478fb8
MATEKUNA <kuna.mate@parlament.hu>
2022-11-28 12:50:29 +0100

    reverted Card wrapper temporarily to fix later in Expenses and ExpenseItem

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -9,14 +9,14 @@ const ExpenseItem = (props) => {
         // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
         // expense-item styles wont apply as default, because component only support, what I tell it to support.
         // I used className='card' in Card, * I have to expand that code to apply styles as well, which was defined here in the props (expense-item)
-        <Card className="expense-item">
+        <div className="expense-item">
             <ExpenseDate date={props.date} />
 
             <div className="expense-item__description"> 
             <h2> {props.title} </h2>
             <div className="expense-item__price"> ${props.amount}</div>
             </div>
-        </Card>
+        </div>
     );
 }
 
