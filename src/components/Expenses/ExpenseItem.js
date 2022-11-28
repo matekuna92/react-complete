732f28b469dbe2e1a2a4336b507584ba9e30da67
MATEKUNA <kuna.mate@parlament.hu>
2022-11-28 13:07:04 +0100

    why updating title in a function doesnt work example

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,13 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+    let title = props.title;
+    const clickHandler = () => {
+        title = 'Updated!';
+        console.log(title);
+    }
+
     // expenseDate is a date object, cant be output as text: toISOString()
     return (
         // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
@@ -13,10 +20,15 @@ const ExpenseItem = (props) => {
             <ExpenseDate date={props.date} />
 
             <div className="expense-item__description"> 
-            <h2> {props.title} </h2>
+            <h2> {title} </h2>
             <div className="expense-item__price"> ${props.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
+
+        // pointing to the clickHandler, not executing it!! ( clickHandler() ) -> with parenthesis it would be executed, when
+        // the button line of code is being parsed (when the return happens, too early! - not when the button is clicked!) 
+
     );
 }
 
