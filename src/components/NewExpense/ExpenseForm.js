9f102ebdf24857b8b9232e6bbbef8ac93705612b
MATEKUNA <kuna.mate@parlament.hu>
2022-11-28 15:01:28 +0100

    one state instead of multiply useStates example

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,23 +3,43 @@ import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+  //  const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  //  const [enteredDate, setEnteredDate] = useState('');
+
+  // using 1 state instead of multiply states: have to update all properties when called
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  })
 
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-        console.log(enteredTitle);
+      //  setEnteredTitle(event.target.value);
+     //   console.log(enteredTitle);
+     setUserInput({
+        ...userInput,           // have to copy the other properties we didnt set, so they wont be lost
+        enteredTitle: event.target.value, // if we only set the Title, other properties would be lost, have to set all the 3
+        // using multiply useStates is better, properties can be updated seperately with the different setters
+     })
     }
 
     const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-        console.log(enteredAmount);
+     //   setEnteredAmount(event.target.value);
+     //   console.log(enteredAmount);
+     setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value, 
+     })
     }
 
     const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-        console.log(enteredDate);
+     //  setEnteredDate(event.target.value);
+     //   console.log(enteredDate);
+     setUserInput({
+        ...userInput,           
+        enteredDate: event.target.value,
+     })
     }
 
     return (
