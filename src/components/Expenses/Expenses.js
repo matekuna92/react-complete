dcffb409d533873d27fd9ea9ed243f0ef43e276d
matekuna92 <kunamate66@gmail.com>
2022-12-04 11:03:34 +0100

    using the year stored in state as value of select in ExpenseFilter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,7 @@ const Expenses = (props) => {
 	return (
 
 		<div className="expenses">
-            <ExpensesFilter onFilterYear={filterYearHandler} />
+            <ExpensesFilter onFilterYear={filterYearHandler} selected={filteredYear} />
             <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
 	        <ExpenseItem title= {expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
 	        <ExpenseItem title= {expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -11,7 +11,7 @@ const ExpensesFilter = (props) => {
         <div className='expenses-filter'>
             <div className='expense-filter__control'>
                 <label> Filter by year </label>
-                <select onChange={handleSelectChange}>
+                <select value={props.selected} onChange={handleSelectChange}>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
