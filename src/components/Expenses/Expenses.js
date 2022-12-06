a13fd24fe2906031bbde124f17b4340b505a4df4
matekuna <kuna.mate@parlament.hu>
2022-12-06 15:21:31 +0100

    ExpenseItem map list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -13,14 +13,16 @@ const Expenses = (props) => {
         console.log('state:', filteredYear);
     }
 
-	return (
-
-		<div className="expenses">
-            <ExpensesFilter onFilterYear={filterYearHandler} selected={filteredYear} />
-            <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-	        <ExpenseItem title= {expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-	        <ExpenseItem title= {expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-	        <ExpenseItem title= {expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
+    return (
+            <div className="expenses">
+                <ExpensesFilter onFilterYear={filterYearHandler} selected={filteredYear} />
+                {expenses.map(expense => (
+                    <ExpenseItem
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                    ))}
 		</div>
 	)
 }
