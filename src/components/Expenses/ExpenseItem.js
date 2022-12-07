eedf2627ede5e15a046cf78b80d3261d9a67da4a
MATEKUNA <kuna.mate@parlament.hu>
2022-12-07 10:56:01 +0100

    refactor list into ExpensesList component, semantics: <li> wrapper in ExpenseItem, since its inside an <ul> now

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,17 +6,19 @@ import Card from '../UI/Card';
 const ExpenseItem = (props) => {
     // expenseDate is a date object, cant be output as text: toISOString()
     return (
-        // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
-        // expense-item styles wont apply as default, because component only support, what I tell it to support.
-        // I used className='card' in Card, * I have to expand that code to apply styles as well, which was defined here in the props (expense-item)
-        <div className="expense-item">
-            <ExpenseDate date={props.date} />
-
-            <div className="expense-item__description"> 
-            <h2> {props.title} </h2>
-            <div className="expense-item__price"> ${props.amount}</div>
+        <li>
+            // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
+            // expense-item styles wont apply as default, because component only support, what I tell it to support.
+            // I used className='card' in Card, * I have to expand that code to apply styles as well, which was defined here in the props (expense-item)
+            <div className="expense-item">
+                <ExpenseDate date={props.date} />
+    
+                <div className="expense-item__description"> 
+                <h2> {props.title} </h2>
+                <div className="expense-item__price"> ${props.amount}</div>
+                </div>
             </div>
-        </div>
+        </li>
 
         // pointing to the clickHandler, not executing it!! ( clickHandler() ) -> with parenthesis it would be executed, when
         // the button line of code is being parsed (when the return happens, too early! - not when the button is clicked!) 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
+
 import './Expenses.css';
-import ExpenseItem from '../Expenses/ExpenseItem';
-import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 	const expenses = props.expenseItems;
@@ -18,26 +18,13 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p> No expenses found in selected year </p>;
-
-    if(filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map(expense => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ));
-    }
-
     // * && js trick: right part of the && is returned, when the left part is true
     // ternary alternative: {filteredExpenses.length === 0 ? (<p> No expenses found in selected year </p>) : ....
     return (
         <div>
             <div className="expenses">
                 <ExpensesFilter onFilterYear={filterYearHandler} selected={filteredYear} />
-                {expensesContent}
+                <ExpensesList items={filteredExpenses} />
     		</div>
         </div>
 	);
