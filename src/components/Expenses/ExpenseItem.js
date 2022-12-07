7d2783de45f1506f660f160adb915e2aed6cfb50
MATEKUNA <kuna.mate@parlament.hu>
2022-12-07 13:55:17 +0100

    hide expenseForm onCancel

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,11 +5,12 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
     // expenseDate is a date object, cant be output as text: toISOString()
+
+    // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
+    // expense-item styles wont apply as default, because component only support, what I tell it to support.
+    // I used className='card' in Card, * I have to expand that code to apply styles as well, which was defined here in the props (expense-item)
     return (
         <li>
-            // cant use custom components as a Wrapper! Need to define props.children in Card.js to make it work
-            // expense-item styles wont apply as default, because component only support, what I tell it to support.
-            // I used className='card' in Card, * I have to expand that code to apply styles as well, which was defined here in the props (expense-item)
             <div className="expense-item">
                 <ExpenseDate date={props.date} />
     
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = (props) => {
+    // form should be visible or not
+    const [isEditing, setIsEditing] = useState(false);
+    let expenseFormContent = '';
 
     // child to parent Communication: Getting expenseData from child ExpenseForm
     // enteredExpenseData parameter will be the returned "expenseData" data from ExpenseForm, when the user submitted the form
@@ -13,13 +16,28 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
 
-       // console.log(expenseData);
         props.onAddExpense(expenseData);
     };
 
+    const startExpenseEditHandler = () => {
+        setIsEditing(true);
+    }
+
+    const stopExpenseEditHandler = () => {
+        setIsEditing(false);
+    }
+
+    if(isEditing) {
+        expenseFormContent = <ExpenseForm
+            onSaveExpenseData={saveExpenseDataHandler}
+            onCancel={stopExpenseEditHandler}
+        />
+    }
+
     return (
         <div className='new-expense'>
-           <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} /> 
+            <button onClick={startExpenseEditHandler}> Add New Expense </button>
+            {expenseFormContent}
         </div>
     );
 };
